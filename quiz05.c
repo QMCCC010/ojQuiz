be2366edf0004ced957d792f6f247387206960bb
QMCCC <2868916930@qq.com>
2025-09-15 21:21:43 +0800

    问题5开头

---
+++ quiz05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct {
+    int x;
+    int y;
+    int r;
+} Satellite;
+
+int main(){
+    return 0;
+    int n, x, y, r = 0;
+    scanf("%d", &n);
+    Satellite *satellite = (Satellite*)malloc(n * sizeof(Satellite));
+}
