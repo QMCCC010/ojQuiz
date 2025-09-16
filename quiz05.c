6e372115da2833dc07722468d970c2f41b6c7f92
QMCCC <2868916930@qq.com>
2025-09-16 10:08:32 +0800

    问题5部分解答

--- quiz05.c
+++ quiz05.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+#define MAX_NUM 200
 typedef struct {
     int x;
     int y;
@@ -9,6 +11,36 @@ typedef struct {
 int main(){
     return 0;
     int n, x, y, r = 0;
+    int max_x, max_y = 0;
+    int counts = 0;
     scanf("%d", &n);
     Satellite *satellite = (Satellite*)malloc(n * sizeof(Satellite));
+
+    // 输入各个卫星的数据
+    for(int i = 0; i < n; i++){
+        scanf("%d %d %d", &x, &y, &r);
+        satellite[i].x = x;
+        satellite[i].y = y;
+        satellite[i].r = r;
+    }
+
+    // 寻找坐标边界
+    for(int i = 0; i < n; i++){
+        if(satellite[i].x + satellite[i].r > max_x) max_x = satellite[i].x + satellite[i].r;
+        if(satellite[i].y + satellite[i].r > max_y) max_y = satellite[i].y + satellite[i].r;
+    }
+
+    // 判断矩阵
+    int *pointLocation = (int*)calloc(max_x * max_y, sizeof(int));
+
+    // 计算可扫描坐标点
+    for(int i = 0; i < n; i++){
+        for(y = satellite[i].y - satellite[i].r; y <= satellite[i].y + satellite[i].r; y++){
+            
+        }
+    }
+
+    free(satellite);
+    free(pointLocation);
+    return 0;
 }
