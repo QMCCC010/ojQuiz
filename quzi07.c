b0a1dfb461ce4a351cf10f8d8dcf2de36ddcd4ca
QMCCC <2868916930@qq.com>
2025-09-16 16:40:33 +0800

    完成问题7

--- quzi07.c
+++ quzi07.c
@@ -1,6 +1,7 @@
 /*
 对于所有异常值，应满足以下条件：
 1、所有元素之和减去异常值为偶数
+2、剩下的元素中存在表示正常值之和的数据
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,17 +9,37 @@
 #define MAX_LEN 10000
 #define MIN_NUM -1000
 #define MAX_NUM 1000
+
 int main(){
     int n = 0;
-    int total = 0;
+    int total = 0; // 所有元素之和
+    int sum = 0; // 表示正常值之和的数据
     scanf("%d", &n);
     int *nums = (int*)malloc(n * sizeof(int));
+    int *outlier = (int*)malloc(n * sizeof(int));
+    int outlierCouts = 0;
+
     for(int i = 0; i < n; i++){
         scanf("%d", &nums[i]);
         // 计算所有元素之和（包括正常值，正常值的和以及异常值）
         total += nums[i];
     }
+    for(int i = 0; i < n; i++){
+        if((total - nums[i]) % 2 == 0){
+            sum = (total - nums[i]) / 2;
+            for(int j = 0; j < n && j != i; j++){
+                if(nums[j] == sum) outlier[outlierCouts++] = nums[i];
+            }
+        }
+    }
+    int maxOutlier = outlier[0];
+    for(int i = 1; i <outlierCouts; i++){
+        if(outlier[i] > maxOutlier) maxOutlier = outlier[i];
+    }
+
+    printf("%d", maxOutlier);
 
     free(nums);
+    free(outlier);
     return 0;
 }
