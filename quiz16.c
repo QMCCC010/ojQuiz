467dce70f91bb7a217fede53cb16c718f5fb343f
QMCCC <2868916930@qq.com>
2025-10-10 19:19:46 +0800

    无

---
+++ quiz16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int factorial(int x){
+    if(x == 0 || x == 1) return 1;
+    else return x * factorial(x - 1);
+}
+
+int ways(int m, int n){
+    return 
+}
+
+int main(){
+    int t = 0;
+    int m[10] = {0}, n[10] = {0};
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++) scanf("%d %d", &m[i], &n[i]);
+}
